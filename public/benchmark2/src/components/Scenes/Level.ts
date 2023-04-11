e5bae7444053ee39bfdbd69da2d5f34730e6e08a
Dongha <61765890+Modem924@users.noreply.github.com>
2023-04-11 15:43:04 -0400

    lvs

---
+++ public/benchmark2/src/components/Scenes/Level.ts
@@ -0,0 +1,60 @@
+import AABB from "../../Wolfie2D/DataTypes/Shapes/AABB";
+import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
+import GameEvent from "../../Wolfie2D/Events/GameEvent";
+import { GameEventType } from "../../Wolfie2D/Events/GameEventType";
+import Input from "../../Wolfie2D/Input/Input";
+import { TweenableProperties } from "../../Wolfie2D/Nodes/GameNode";
+import { GraphicType } from "../../Wolfie2D/Nodes/Graphics/GraphicTypes";
+import Rect from "../../Wolfie2D/Nodes/Graphics/Rect";
+import AnimatedSprite from "../../Wolfie2D/Nodes/Sprites/AnimatedSprite";
+import OrthogonalTilemap from "../../Wolfie2D/Nodes/Tilemaps/OrthogonalTilemap";
+import Label from "../../Wolfie2D/Nodes/UIElements/Label";
+import { UIElementType } from "../../Wolfie2D/Nodes/UIElements/UIElementTypes";
+import RenderingManager from "../../Wolfie2D/Rendering/RenderingManager";
+import Scene from "../../Wolfie2D/Scene/Scene";
+import SceneManager from "../../Wolfie2D/Scene/SceneManager";
+import Viewport from "../../Wolfie2D/SceneGraph/Viewport";
+import Timer from "../../Wolfie2D/Timing/Timer";
+import Color from "../../Wolfie2D/Utils/Color";
+import { EaseFunctionType } from "../../Wolfie2D/Utils/EaseFunctions";
+//import PlayerController, { PlayerTweens } from "../Player/PlayerController";
+//import PlayerWeapon from "../Player/PlayerWeapon";
+
+//import { HW3Events } from "../HW3Events";
+//import { HW3PhysicsGroups } from "../HW3PhysicsGroups";
+//import HW3FactoryManager from "../Factory/HW3FactoryManager";
+import MainMenu from "./MainMenu";
+import Particle from "../../Wolfie2D/Nodes/Graphics/Particle";
+/**
+ * A const object for the layer names
+ */
+export const HWLayers = {
+    // The primary layer
+    PRIMARY: "PRIMARY",
+    // The UI layer
+    UI: "UI"
+} as const;
+
+
+export type HWLayer = typeof HWLayers[keyof typeof HWLayers]
+
+export default abstract class Level extends Scene {
+    protected tilemapKey: string;
+    protected destructibleLayerKey: string;
+    protected wallsLayerKey: string;
+    /** The scale for the tilemap */
+    protected tilemapScale: Vec2;
+    /** The destrubtable layer of the tilemap */
+    protected destructable: OrthogonalTilemap;
+    /** The wall layer of the tilemap */
+    protected walls: OrthogonalTilemap;
+
+    public startScene(): void {
+        // Initialize the layers
+        this.initLayers();
+
+        // Initialize the tilemaps
+        this.initializeTilemap();
+    }
+
+}
--- public/benchmark2/src/components/Scenes/Level1.ts
+++ public/benchmark2/src/components/Scenes/Level1.ts
@@ -1,8 +1,27 @@
-import Scene from "../../Wolfie2D/Scene/Scene";
+import AABB from "../../Wolfie2D/DataTypes/Shapes/AABB";
+import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
+import Level from "./Level";
+import RenderingManager from "../../Wolfie2D/Rendering/RenderingManager";
+import SceneManager from "../../Wolfie2D/Scene/SceneManager";
+import Viewport from "../../Wolfie2D/SceneGraph/Viewport";
 
-export default class Level1 extends Scene {
 
+export default class Level1 extends Level {
 
-    
+    public static readonly TILEMAP_KEY = "LEVEL1";
+    public static readonly TILEMAP_PATH = "assets/tilemaps/level1.json";
+    public static readonly TILEMAP_SCALE = new Vec2(2, 2);
+
+    public constructor(viewport: Viewport, sceneManager: SceneManager, renderingManager: RenderingManager, options: Record<string, any>) {
+        super(viewport, sceneManager, renderingManager, options);
+
+        this.tilemapKey = Level1.TILEMAP_KEY;
+        this.tilemapScale = Level1.TILEMAP_SCALE;
+    }
+
+    public loadScene(): void {
+        // Load in the tilemap
+        this.load.tilemap(this.tilemapKey, Level1.TILEMAP_PATH);
+    }
 
 }
