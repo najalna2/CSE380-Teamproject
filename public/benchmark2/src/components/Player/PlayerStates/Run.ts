aa3ccf7d6f096418036d5ec91fe51042abb2e2ef
Dongha <61765890+Modem924@users.noreply.github.com>
2023-04-24 23:34:26 -0400

    update

--- public/benchmark2/src/components/Player/PlayerStates/Run.ts
+++ public/benchmark2/src/components/Player/PlayerStates/Run.ts
@@ -7,7 +7,7 @@ export default class Run extends PlayerState {
 
 	onEnter(options: Record<string, any>): void {
 		this.parent.speed = this.parent.MIN_SPEED;
-        this.owner.animation.playIfNotAlready(PlayerAnimations.RUN);
+        this.owner.animation.playIfNotAlready(PlayerAnimations.WALK);
 	}
 
 	update(deltaT: number): void {
